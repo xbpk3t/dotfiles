5b9cd5315165f805c7a8dbd801015bb7da2953bc
XBPk3T <yyzw@live.com>
2025-07-10 22:42:55 +0800

    feat(nix): add nix

---
+++ nix-darwin/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, ... }: {
+      # Import our main configuration
+      imports = [ ./configuration.nix ];
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#lHGtQdeMacBook-Pro-2
+    darwinConfigurations."lHGtQdeMacBook-Pro-2" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "lhgtqb7bll";
+          };
+        }
+      ];
+    };
+  };
+}
