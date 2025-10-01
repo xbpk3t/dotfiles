adfc658466aa281f90a0710b96eca5943135ff5a
xbpk3t <>
2025-10-01 11:14:39 +0800

    fix: 用 vicinae 代替 anyrun 作为 launcher

--- home/base/tui/default.nix
+++ home/base/tui/default.nix
@@ -1,23 +1,3 @@
-{...}: {
-  imports = [
-    ./bash.nix
-    ./btop.nix
-    ./cc.nix
-    ./direnv.nix
-    ./fastfetch.nix
-    ./fzf.nix
-    ./git.nix
-    ./gpg.nix
-    ./helix.nix
-    ./jq.nix
-    ./keymap.nix
-    ./nvim.nix
-    ./nix.nix
-    ./pandoc.nix
-    ./settings.nix
-    ./taskfile.nix
-    ./uv.nix
-    ./yazi.nix
-    ./zellij.nix
-  ];
+{mylib, ...}: {
+  imports = mylib.scanPaths ./.;
 }
