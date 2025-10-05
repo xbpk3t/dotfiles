e5c64c0ba519a2584d6fc48fec5770ff25258e81
xbpk3t <>
2025-10-05 21:44:02 +0800

    feat(wezterm): 自己调整wezterm配置

--- home/base/gui/wezterm.nix
+++ home/base/gui/wezterm.nix
@@ -3,26 +3,11 @@
   programs.wezterm = {
     enable = true;
     enableZshIntegration = true;
-
-    extraConfig = ''
-      local wezterm = require("wezterm")
-      local config = wezterm.config_builder()
-
-      -- 设置 Lua 模块搜索路径指向配置目录
-      package.path = package.path .. ";${config.xdg.configHome}/wezterm/?.lua"
-
-      -- 加载各个模块
-      require("appearance")(wezterm, config)
-      require("keybings")(wezterm, config)
-      require("status")(wezterm, config)
-      require("tabs")(wezterm, config)
-
-      return config
-    '';
   };
 
   # 将 Lua 模块文件部署到配置目录
   xdg.configFile = {
+    "wezterm/wezterm.lua".source = ./wezterm/wezterm.lua;
     "wezterm/appearance.lua".source = ./wezterm/appearance.lua;
     "wezterm/keybings.lua".source = ./wezterm/keybings.lua;
     "wezterm/status.lua".source = ./wezterm/status.lua;
