a3b961605390d7c509fd3891378658c6b1d01e0a
xbpk3t <yyzw@live.com>
2025-10-10 16:41:56 +0800

    fix(home): 真正处理掉 taskfile和hs 实现类似dotbot那种直接软链接到HOME下，实现双向修改

--- home/darwin/apps/hs.nix
+++ home/darwin/apps/hs.nix
@@ -1,4 +1,7 @@
-{config, ...}: {
-  home.file.".hammerspoon".source = config.lib.file.mkOutOfStoreSymlink ./.hammerspoon;
+{config,myvars, ...}: {
+#  home.file.".hammerspoon".source = config.lib.file.mkOutOfStoreSymlink ./.hammerspoon;
+
+  #
+  home.file.".hammerspoon".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Desktop/${myvars.name}/.hammerspoon";
   home.file.".hammerspoon".recursive = true;
 }
--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,10 +1,10 @@
 {
   myvars,
   lib,
+  mylib,
   ...
 }: {
-  # This module provides the base home-manager configuration
-  # that will be imported by the system configuration
+  imports = [../base] ++ mylib.scanPaths ./.;
 
   # FIXME [2025-10-08] 查一下为啥有这么多 stateVersion，能否只定义一次？
   home = {
