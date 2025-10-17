f25e5f700f470ef39016293245ead9f23daa63fe
xbpk3t <yyzw@live.com>
2025-10-17 22:46:01 +0800

    fix(fcitx5): fix之前fcitx5的配置问题
    home-manager-files> Error installing file '.config/fcitx5/conf/cloudpinyin.conf' outside

---
+++ home/base/core/lazygit.nix
@@ -0,0 +1,58 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    lazygit
+  ];
+
+  # https://mynixos.com/nixpkgs/options/programs.lazygit
+  programs.lazygit = {
+    enable = true;
+    package = pkgs.lazygit;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "diff-so-fancy";
+        };
+        disableForcePushing = true;
+      };
+      gui = {
+        language = "en";
+        mouseEvents = false;
+        sidePanelWidth = 0.3;
+        mainPanelSplitMode = "flexible"; # one of "horizontal" | "flexible" | "vertical"
+        showFileTree = false; # ` to toggle
+        nerdFontsVersion = "3";
+        commitHashLength = 6;
+        showDivergenceFromBaseBranch = "arrowAndNumber";
+      };
+      quitOnTopLevelReturn = true;
+      disableStartupPopups = true;
+      promptToReturnFromSubprocess = false;
+      os = {editPreset = "nvim";};
+      keybinding = {
+        files = {
+          stashAllChanges = "<c-a>"; # instead of just 's' which I typod for 'c'
+        };
+        universal = {
+          prevItem = "e";
+          nextItem = "n";
+          scrollUpMain = "<up>"; # main panel scroll up
+          scrollDownMain = "<down>"; # main panel scroll down
+          nextMatch = "j";
+          prevMatch = "J";
+          new = "<c-a>";
+          edit = "<c-r>";
+        };
+      };
+      # https://github.com/jesseduffield/lazygit/blob/master/docs/Custom_Command_Keybindings.md
+      customCommands = [
+        {
+          key = "H";
+          context = "commits";
+          # or use "y u" to copy the url
+          command = "gh browse {{.SelectedLocalCommit.Hash}}";
+        }
+      ];
+    };
+  };
+}
