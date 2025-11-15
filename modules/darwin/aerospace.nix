bb2fe712e53c090b5b1c643f7bb4d27952127114
xbpk3t <yyzw@live.com>
2025-11-15 11:03:58 +0800

    feat(misc): 杂项提交

--- modules/darwin/aerospace.nix
+++ modules/darwin/aerospace.nix
@@ -1,5 +1,10 @@
 _: {
+  # https://mynixos.com/nixpkgs/package/aerospace
   # https://mynixos.com/nix-darwin/options/services.aerospace
+
+  # jankyborders macos专用，用来高亮当前window和 aerospace 等window管理器集成
+  # https://mynixos.com/nixpkgs/package/jankyborders
+  # https://mynixos.com/nix-darwin/options/services.jankyborders
   services.aerospace = {
     enable = true;
   };
