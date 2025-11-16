e23204a7842fa69ef65821622c0b6baa84648683
xbpk3t <yyzw@live.com>
2025-11-16 21:04:03 +0800

    feat(nixos-vps): 用 nixos-anywhere 把VPS刷成 NixOS.  fix #20

---
+++ hosts/nixos-vps/hardware.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "virtio_pci"
+    "virtio_blk"
+    "virtio_scsi"
+    "ahci"
+    "xhci_pci"
+    "sd_mod"
+  ];
+
+  boot.initrd.kernelModules = ["virtio_net"];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
--- modules/nixos/desktop/time.nix
+++ modules/nixos/desktop/time.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
+  # 启动 `services.tuptime` 计时器 | 适合桌面统计 uptime，对服务器没有必需价值，却额外拉入 `tuptime` 及 `tuptime-sync.timer`
   environment.systemPackages = with pkgs; [
     tuptime
   ];
