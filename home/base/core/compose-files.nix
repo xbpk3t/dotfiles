9dd78906c0c7bfe5dbfed6f14da23c3a0d008457
xbpk3t <yyzw@live.com>
2025-11-28 21:59:29 +0800

    fix(zsh): 解决掉用 colmena 部署之后 taskfile 无法在目标机器上使用的问题
    ---
    
    用 mylib.relativeToRoot 替代
    
    之前使用的 config.lib.file 以及  config.lib.file.mkOutOfStoreSymlink
    
    这两种方案
    
    使用 config.lib.file.mkOutOfStoreSymlink 则在目标机器上该文件不存在
    
    使用 config.lib.file，该文件存在，但是本地文件不可写
    
    之前的处理始终有问题
    
    使用 mylib.relativeToRoot 后，已验证上述问题均已解决

--- home/base/core/compose-files.nix
+++ home/base/core/compose-files.nix
@@ -1,10 +1,9 @@
-{mylib, ...}: let
-  cntrSource = mylib.relativeToRoot ".cntr";
-in {
+{mylib, ...}: {
   # Ship the entire cntr directory into the user's home so docker compose
   # stacks can be managed imperatively on any host without relying on a local checkout.
   home.file.".cntr" = {
-    source = cntrSource;
+    source = mylib.relativeToRoot ".cntr";
     recursive = true;
+    force = true;
   };
 }
