0afe571d36438a3d1846d9e5efe67dba1a31cd6e
xbpk3t <yyzw@live.com>
2025-11-28 21:59:29 +0800

    fix(zsh): 解决掉用 colmena 部署之后 taskfile 无法在目标机器上使用的问题
    ---
    
    用 mylib.relativeToRoot 替代
    
    之前使用的 config.lib.file 以及  config.lib.file.mkOutOfStoreSymlink
    
    这两种方案
    
    使用 config.lib.file.mkOutOfStoreSymlink 则在目标机器上该文件不存在
    
    使用 config.lib.file，该文件存在，但是本地文件不可写
    
    之前的处理始终有问题
    
    使用 mylib.relativeToRoot 后，已验证上述问题均已解决

--- home/base/core/taskfile.nix
+++ home/base/core/taskfile.nix
@@ -1,18 +1,24 @@
 {
-  config,
   pkgs,
-  myvars,
+  mylib,
   ...
 }: {
   # 使用 mkOutOfStoreSymlink 创建指向 taskfile 目录和 Taskfile.yml 文件的符号链接
   home.file."taskfile" = {
-#    source = config.lib.file.mkOutOfStoreSymlink "${myvars.projectRoot}/taskfile";
-    source = "${myvars.projectRoot}/taskfile";
+    # source = config.lib.file.mkOutOfStoreSymlink "${myvars.projectRoot}/taskfile";
+    # source = "${myvars.projectRoot}/taskfile";
+
+    source = mylib.relativeToRoot "taskfile";
     recursive = true;
+    force = true;
   };
 
-#  home.file."Taskfile.yml".source = config.lib.file.mkOutOfStoreSymlink "${myvars.projectRoot}/taskfile/Taskfile.yml";
-  home.file."Taskfile.yml".source = "${myvars.projectRoot}/taskfile/Taskfile.yml";
+  home.file."Taskfile.yml" = {
+    # source = config.lib.file.mkOutOfStoreSymlink "${myvars.projectRoot}/taskfile/Taskfile.yml";
+
+    source = mylib.relativeToRoot "taskfile/Taskfile.yml";
+    force = true;
+  };
 
   home.packages = with pkgs; [
     go-task
