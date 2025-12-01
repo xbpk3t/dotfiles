c504499258b6622508746d4bb655b7fba9ed6a14
xbpk3t <yyzw@live.com>
2025-12-01 11:27:18 +0800

    build(flake): 处理掉所有rebuild时的warning
    ---
    
    ```markdown
    
    evaluation warning: You have set specialArgs.pkgs, which means that options like nixpkgs.config
                        and nixpkgs.overlays will be ignored. If you wish to reuse an already created
                        pkgs, which you know is configured correctly for this NixOS configuration,
                        please import the `nixosModules.readOnlyPkgs` module from the nixpkgs flake or
                        `(modulesPath + "/misc/nixpkgs/read-only.nix"), and set `{ nixpkgs.pkgs = <your pkgs>; }`.
                        This properly disables the ignored options to prevent future surprises.
    evaluation warning: 'system' has been renamed to/replaced by 'stdenv.hostPlatform.system'
    evaluation warning: luck profile: You are using different Stylix and Home Manager versions. This is
                        likely to cause errors and unexpected behavior. It is highly
                        recommended that you use a version of Stylix that matches your chosen
                        version of Home Manager.
    
                        If you are willing to accept the risks that come with using
                        mismatched versions, you may disable this warning by adding
    
                            stylix.enableReleaseChecks = false;
    
                        to your configuration.
    evaluation warning: luck profile: The option `programs.vscode.extensions' defined in `/nix/store/rsfwj5rka64fy4523836i1rlc1rb2lg9-source/home/nixos/gui/IDE.nix' has been renamed to `programs.vscode.profiles.default.extensions'.
    evaluation warning: luck profile: The option `programs.vscode.userSettings' defined in `/nix/store/rsfwj5rka64fy4523836i1rlc1rb2lg9-source/home/nixos/gui/IDE.nix' has been renamed to `programs.vscode.profiles.default.userSettings'.
    evaluation warning: luck profile: The option `programs.vscode.enableExtensionUpdateCheck' defined in `/nix/store/rsfwj5rka64fy4523836i1rlc1rb2lg9-source/home/nixos/gui/IDE.nix' has been renamed to `programs.vscode.profiles.default.enableExtensionUpdateCheck'.
    evaluation warning: luck profile: The option `programs.vscode.enableUpdateCheck' defined in `/nix/store/rsfwj5rka64fy4523836i1rlc1rb2lg9-source/home/nixos/gui/IDE.nix' has been renamed to `programs.vscode.profiles.default.enableUpdateCheck'.
    evaluation warning: luck profile: The option `programs.git.aliases' defined in `/nix/store/rsfwj5rka64fy4523836i1rlc1rb2lg9-source/home/base/core/git.nix' has been renamed to `programs.git.settings.alias'.
    evaluation warning: luck profile: The option `programs.git.diff-so-fancy.enable' defined in `/nix/store/rsfwj5rka64fy4523836i1rlc1rb2lg9-source/home/base/core/git.nix' has been renamed to `programs.diff-so-fancy.enable'.
    evaluation warning: luck profile: `programs.diff-so-fancy.enableGitIntegration` automatic enablement is deprecated. Please explicitly set `programs.diff-so-fancy.enableGitIntegration = true`.
    evaluation warning: luck profile: You have set either `nixpkgs.config` or `nixpkgs.overlays` while using `home-manager.useGlobalPkgs`.
                        This will soon not be possible. Please remove all `nixpkgs` options when using `home-manager.useGlobalPkgs`.
    ```

--- home/nixos/gui/IDE.nix
+++ home/nixos/gui/IDE.nix
@@ -302,181 +302,184 @@ in {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    enableUpdateCheck = true;
-    enableExtensionUpdateCheck = true;
 
     # 保证 settings, ext, keybinds 相应配置文件可写
     mutableExtensionsDir = true;
     # mutableSettings = true;
     # mutableKeybindings = true;
 
-    extensions = with pkgs.vscode-extensions; [
-      tuttieee.emacs-mcx
+    profiles.default = {
+      enableUpdateCheck = true;
+      enableExtensionUpdateCheck = true;
 
-      # https://mynixos.com/nixpkgs/package/vscode-extensions.ziglang.vscode-zig
-      # https://mynixos.com/nixpkgs/package/vscode-extensions.tiehuis.zig
-      # used to replace tiehuis.zig
-      ziglang.vscode-zig
+      extensions = with pkgs.vscode-extensions; [
+        tuttieee.emacs-mcx
 
-      rust-lang.rust-analyzer
-      kahole.magit
-      graphql.vscode-graphql
-      catppuccin.catppuccin-vsc
-      bbenoist.nix
-      jnoortheen.nix-ide
-      golang.go
-      ms-python.python
-      redhat.vscode-yaml
-      ms-azuretools.vscode-docker
-      ms-vscode.cpptools
-      hashicorp.terraform
-      tamasfe.even-better-toml
-      timonwong.shellcheck
-      dbaeumer.vscode-eslint
-      esbenp.prettier-vscode
-      streetsidesoftware.code-spell-checker
-      eamodio.gitlens
-      # vscodevim.vim
+        # https://mynixos.com/nixpkgs/package/vscode-extensions.ziglang.vscode-zig
+        # https://mynixos.com/nixpkgs/package/vscode-extensions.tiehuis.zig
+        # used to replace tiehuis.zig
+        ziglang.vscode-zig
 
-      ms-vscode.makefile-tools
-      ms-vscode-remote.remote-ssh
-      ms-vscode-remote.remote-ssh-edit
-      ms-vscode-remote.remote-containers
-      github.vscode-pull-request-github
-      ibm.output-colorizer
-      oderwat.indent-rainbow
-      formulahendry.code-runner
-      gruntfuggly.todo-tree
-    ];
+        rust-lang.rust-analyzer
+        kahole.magit
+        graphql.vscode-graphql
+        catppuccin.catppuccin-vsc
+        bbenoist.nix
+        jnoortheen.nix-ide
+        golang.go
+        ms-python.python
+        redhat.vscode-yaml
+        ms-azuretools.vscode-docker
+        ms-vscode.cpptools
+        hashicorp.terraform
+        tamasfe.even-better-toml
+        timonwong.shellcheck
+        dbaeumer.vscode-eslint
+        esbenp.prettier-vscode
+        streetsidesoftware.code-spell-checker
+        eamodio.gitlens
+        # vscodevim.vim
 
-    userSettings = {
-      "breadcrumbs.enabled" = true;
-      "diffEditor.ignoreTrimWhitespace" = false;
-      "editor.bracketPairColorization.enabled" = true;
-      "editor.codeActionsOnSave" = {
-        "source.fixAll" = "explicit";
-        "source.organizeImports" = "explicit";
-      };
-      "editor.cursorSmoothCaretAnimation" = "on";
-      #      "editor.fontFamily" = "Sarasa Mono SC";
-      "editor.fontLigatures" = true;
-      #      "editor.fontSize" = 13;
-      "editor.formatOnPaste" = true;
-      "editor.formatOnSave" = true;
-      "editor.guides.bracketPairs" = "active";
-      "editor.inlineSuggest.enabled" = true;
-      "editor.linkedEditing" = true;
-      "editor.minimap.enabled" = false;
-      "editor.renderWhitespace" = "boundary";
-      "editor.rulers" = [80 100 120];
-      "editor.smoothScrolling" = true;
-      "editor.stickyScroll.enabled" = true;
-      "editor.tabSize" = 2;
-      "editor.wordWrap" = "bounded";
-      "editor.wordWrapColumn" = 120;
+        ms-vscode.makefile-tools
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        ms-vscode-remote.remote-containers
+        github.vscode-pull-request-github
+        ibm.output-colorizer
+        oderwat.indent-rainbow
+        formulahendry.code-runner
+        gruntfuggly.todo-tree
+      ];
 
-      "explorer.confirmDelete" = false;
-      "explorer.confirmDragAndDrop" = false;
-
-      "extensions.autoCheckUpdates" = false;
-      "extensions.autoUpdate" = false;
-
-      "files.eol" = "\n";
-      "files.insertFinalNewline" = true;
-      "files.trimFinalNewlines" = true;
-      "files.trimTrailingWhitespace" = true;
-      "files.watcherExclude" = {
-        "**/.direnv/**" = true;
-        "**/.jj/**" = true;
-        "**/node_modules/**" = true;
-        "**/target/**" = true;
-      };
-
-      "git.autofetch" = true;
-      "git.confirmSync" = false;
-      "git.enableSmartCommit" = true;
-
-      "gopls" = {
-        "staticcheck" = true;
-        "usePlaceholders" = true;
-      };
-
-      "nix.enableLanguageServer" = true;
-      "nix.formatterPath" = "nixfmt";
-      "nix.serverPath" = "nixd";
-
-      "python.analysis.autoImportCompletions" = true;
-      "python.formatting.provider" = "black";
-
-      "remote.SSH.useLocalServer" = false;
-
-      "rust-analyzer.cargo.buildScripts.enable" = true;
-      "rust-analyzer.check.command" = "clippy";
-
-      "search.exclude" = {
-        "**/.direnv/**" = true;
-        "**/.git/**" = true;
-        "**/node_modules/**" = true;
-        "**/target/**" = true;
-      };
-
-      "security.workspace.trust.untrustedFiles" = "open";
-
-      "telemetry.telemetryLevel" = "off";
-      "terminal.integrated.defaultProfile.linux" = "zsh";
-      "terminal.integrated.drawBoldTextInBrightColors" = false;
-      #      "terminal.integrated.fontFamily" = "Sarasa Mono SC";
-      #      "terminal.integrated.fontSize" = 12;
-
-      "todo-tree.general.tags" = ["TODO" "FIXME" "BUG" "NOTE"];
-      "todo-tree.tree.showScanModeButton" = false;
-
-      "update.mode" = "none";
-      "window.autoDetectColorScheme" = false;
-      "window.commandCenter" = false;
-      "window.titleBarStyle" = "custom";
-      #      "workbench.colorTheme" = "Catppuccin Mocha";
-      "workbench.editor.enablePreview" = false;
-      "workbench.preferredDarkColorTheme" = "Catppuccin Mocha";
-      "workbench.startupEditor" = "none";
-
-      "[go]" = {
+      userSettings = {
+        "breadcrumbs.enabled" = true;
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.bracketPairColorization.enabled" = true;
         "editor.codeActionsOnSave" = {
+          "source.fixAll" = "explicit";
           "source.organizeImports" = "explicit";
         };
-        "editor.defaultFormatter" = "golang.go";
-        "editor.formatOnSaveMode" = "file";
-      };
-      "[graphql]" = {
-        "editor.defaultFormatter" = "graphql.vscode-graphql";
-      };
-      "[json]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[jsonc]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[markdown]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "editor.wordWrap" = "on";
-      };
-      "[nix]" = {
-        "editor.defaultFormatter" = "jnoortheen.nix-ide";
-      };
-      "[python]" = {
-        "editor.defaultFormatter" = "ms-python.python";
-      };
-      "[rust]" = {
-        "editor.defaultFormatter" = "rust-lang.rust-analyzer";
-      };
-      "[toml]" = {
-        "editor.defaultFormatter" = "tamasfe.even-better-toml";
-      };
-      "[typescript]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[yaml]" = {
-        "editor.defaultFormatter" = "redhat.vscode-yaml";
+        "editor.cursorSmoothCaretAnimation" = "on";
+        #      "editor.fontFamily" = "Sarasa Mono SC";
+        "editor.fontLigatures" = true;
+        #      "editor.fontSize" = 13;
+        "editor.formatOnPaste" = true;
+        "editor.formatOnSave" = true;
+        "editor.guides.bracketPairs" = "active";
+        "editor.inlineSuggest.enabled" = true;
+        "editor.linkedEditing" = true;
+        "editor.minimap.enabled" = false;
+        "editor.renderWhitespace" = "boundary";
+        "editor.rulers" = [80 100 120];
+        "editor.smoothScrolling" = true;
+        "editor.stickyScroll.enabled" = true;
+        "editor.tabSize" = 2;
+        "editor.wordWrap" = "bounded";
+        "editor.wordWrapColumn" = 120;
+
+        "explorer.confirmDelete" = false;
+        "explorer.confirmDragAndDrop" = false;
+
+        "extensions.autoCheckUpdates" = false;
+        "extensions.autoUpdate" = false;
+
+        "files.eol" = "\n";
+        "files.insertFinalNewline" = true;
+        "files.trimFinalNewlines" = true;
+        "files.trimTrailingWhitespace" = true;
+        "files.watcherExclude" = {
+          "**/.direnv/**" = true;
+          "**/.jj/**" = true;
+          "**/node_modules/**" = true;
+          "**/target/**" = true;
+        };
+
+        "git.autofetch" = true;
+        "git.confirmSync" = false;
+        "git.enableSmartCommit" = true;
+
+        "gopls" = {
+          "staticcheck" = true;
+          "usePlaceholders" = true;
+        };
+
+        "nix.enableLanguageServer" = true;
+        "nix.formatterPath" = "nixfmt";
+        "nix.serverPath" = "nixd";
+
+        "python.analysis.autoImportCompletions" = true;
+        "python.formatting.provider" = "black";
+
+        "remote.SSH.useLocalServer" = false;
+
+        "rust-analyzer.cargo.buildScripts.enable" = true;
+        "rust-analyzer.check.command" = "clippy";
+
+        "search.exclude" = {
+          "**/.direnv/**" = true;
+          "**/.git/**" = true;
+          "**/node_modules/**" = true;
+          "**/target/**" = true;
+        };
+
+        "security.workspace.trust.untrustedFiles" = "open";
+
+        "telemetry.telemetryLevel" = "off";
+        "terminal.integrated.defaultProfile.linux" = "zsh";
+        "terminal.integrated.drawBoldTextInBrightColors" = false;
+        #      "terminal.integrated.fontFamily" = "Sarasa Mono SC";
+        #      "terminal.integrated.fontSize" = 12;
+
+        "todo-tree.general.tags" = ["TODO" "FIXME" "BUG" "NOTE"];
+        "todo-tree.tree.showScanModeButton" = false;
+
+        "update.mode" = "none";
+        "window.autoDetectColorScheme" = false;
+        "window.commandCenter" = false;
+        "window.titleBarStyle" = "custom";
+        #      "workbench.colorTheme" = "Catppuccin Mocha";
+        "workbench.editor.enablePreview" = false;
+        "workbench.preferredDarkColorTheme" = "Catppuccin Mocha";
+        "workbench.startupEditor" = "none";
+
+        "[go]" = {
+          "editor.codeActionsOnSave" = {
+            "source.organizeImports" = "explicit";
+          };
+          "editor.defaultFormatter" = "golang.go";
+          "editor.formatOnSaveMode" = "file";
+        };
+        "[graphql]" = {
+          "editor.defaultFormatter" = "graphql.vscode-graphql";
+        };
+        "[json]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "[jsonc]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "[markdown]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+          "editor.wordWrap" = "on";
+        };
+        "[nix]" = {
+          "editor.defaultFormatter" = "jnoortheen.nix-ide";
+        };
+        "[python]" = {
+          "editor.defaultFormatter" = "ms-python.python";
+        };
+        "[rust]" = {
+          "editor.defaultFormatter" = "rust-lang.rust-analyzer";
+        };
+        "[toml]" = {
+          "editor.defaultFormatter" = "tamasfe.even-better-toml";
+        };
+        "[typescript]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "[yaml]" = {
+          "editor.defaultFormatter" = "redhat.vscode-yaml";
+        };
       };
     };
   };
