43a57a12cfe8342b8b8d4be731663a5538252c24
xbpk3t <yyzw@live.com>
2025-12-11 00:51:14 +0800

    chore(pkgs): 移除 todui, gnome-macos-remap-wayland 这两个自己打包的pkg，用不到了

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,5 @@
 pkgs: {
-  # spotify-player-git = pkgs.callPackage ./spotify-player { };
-  # yaak-git = pkgs.callPackage ./yaak { };
-  todui = pkgs.callPackage ./todui {};
   apple-pingfang = pkgs.callPackage ./apple-pingfang {};
   ruler = pkgs.callPackage ./ruler {};
   goland-2025-3 = pkgs.callPackage ./goland-2025-3 {};
-  gnome-macos-remap-wayland = pkgs.callPackage ./gnome-macos-remap-wayland {};
-  # gdlauncher = pkgs.callPackage ./gdlauncher { };
-  # discord-rpc-lsp = pkgs.callPackage ./discord-rpc-lsp { };
-  #
 }
