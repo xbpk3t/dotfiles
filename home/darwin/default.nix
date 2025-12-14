f42e540c17469c28e73d14e2fd91f3d1724a87a8
xbpk3t <yyzw@live.com>
2025-12-14 15:26:31 +0800

    feat(hm): 移除 mac-app-util,现在不需要该flake，也可以正确识别通过hm安装的APP了

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,7 +1,6 @@
 {
   myvars,
   mylib,
-  pkgs,
   ...
 }: {
   imports = [../base] ++ mylib.scanPaths ./.;
@@ -9,12 +8,6 @@
   home = {
     username = myvars.username;
     homeDirectory = "/Users/${myvars.username}";
-    # mac-app-util 配置项
-    # TODO 这里能否做一个配置化启用的连动？
-    packages = with pkgs; [
-      alacritty
-      # zed
-    ];
   };
 
   # Let Home Manager install and manage itself.
