c222c7e93f45f3108b87371239b8bed5fdce966a
xbpk3t <yyzw@live.com>
2025-12-14 15:24:32 +0800

    fix(hs): 之前的写法，无法把 .hammerspoon 放到目标path下

--- home/darwin/hs.nix
+++ home/darwin/hs.nix
@@ -1,13 +1,11 @@
-{
-  config,
-  myvars,
-  ...
-}: {
+{mylib, ...}: {
   #  home.file.".hammerspoon".source = config.lib.file.mkOutOfStoreSymlink ./.hammerspoon;
 
   # 这里需要注意 mkOutOfStoreSymlink 后面如果配置为相对路径，会被hm认为是nix的一部分，会被作为 /nix/store 软链接到相应的HOME目录下。如果配置为绝对路径，会被指向到 nix store之外的可变目录
   home.file.".hammerspoon" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${myvars.projectRoot}/.hammerspoon";
+    #    source = config.lib.file.mkOutOfStoreSymlink "${myvars.projectRoot}/.hammerspoon";
+    source = mylib.relativeToRoot ".hammerspoon";
     recursive = true;
+    force = true;
   };
 }
