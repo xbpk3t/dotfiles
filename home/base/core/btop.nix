3183088435aeca32cf6de0d9d42da30e2d89b17a
xbpk3t <yyzw@live.com>
2025-12-25 10:26:49 +0800

    feat(btop): 把 htop 从 zsh.nix 挪到 btop.nix 里

--- home/base/core/btop.nix
+++ home/base/core/btop.nix
@@ -1,4 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
+  # https://github.com/aristocratos/btop
+  # 【技术选型】简单说明，为啥同时使用 btop 和 htp? 以及为什么不需要 glances
+  ##  glances 本质是个services (还是个WebAPP)。但是其核心是“轻量级monitor系统”，glances还分可以分为server模式和client模式，所有slave机器都需要安装glances的server，master开启client模式就可以收集所有slave的数据（类似prometheus这样的pull模式）。所以也可以理解为局域网下的prom（glances不适合在公网做monitor），感觉意思不大。
+  ## htop：更像 Better top，核心是 process + CPU/内存 的交互与操作
+  ## btop：更像 轻量系统仪表盘，把 procs/mem/Disk/net/CPU 放到一屏里。btop的procs里没有 nice、priority、virt/res/shr 这类字段。所以仍然需要htop
+
   programs.btop = {
     enable = true;
     package = pkgs.btop.override {
@@ -19,5 +29,47 @@
       io_mode = true;
       io_graph_combined = false;
     };
+  };
+
+  # https://mynixos.com/home-manager/options/programs.htop
+  programs.htop = {
+    enable = true;
+    settings =
+      {
+        color_scheme = 6;
+        cpu_count_from_one = 0;
+        delay = 15;
+        fields = with config.lib.htop.fields; [
+          PID
+          USER
+          PRIORITY
+          NICE
+          M_SIZE
+          M_RESIDENT
+          M_SHARE
+          STATE
+          PERCENT_CPU
+          PERCENT_MEM
+          TIME
+          COMM
+        ];
+        highlight_base_name = 1;
+        highlight_megabytes = 1;
+        highlight_threads = 1;
+      }
+      // (with config.lib.htop;
+        leftMeters [
+          (bar "AllCPUs2")
+          (bar "Memory")
+          (bar "Swap")
+          (text "Zram")
+        ])
+      // (with config.lib.htop;
+        rightMeters [
+          (text "Tasks")
+          (text "LoadAverage")
+          (text "Uptime")
+          (text "Systemd")
+        ]);
   };
 }
