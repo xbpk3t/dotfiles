ee049b59ff6e51112ed742bd68810670e400aaf4
xbpk3t <yyzw@live.com>
2026-01-04 19:49:39 +0800

    feat(tailscale): netbird -> tailscale 作为本地异地组网方案

--- modules/darwin/netbird-client.nix
+++ modules/darwin/netbird-client.nix
@@ -1,14 +1,30 @@
-{pkgs, ...}: {
-  # 本配置非必需，services.netbird 本身会把install netbird，但是我可能需要手动执行 netbird相关命令，所以添加该配置
-  environment.systemPackages = with pkgs; [
-    # 注意这里应是 netbird，而非 netbird-client
-    netbird
-  ];
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.modules.networking.netbird;
+in {
+  options.modules.networking.netbird.enable =
+    mkEnableOption "NetBird client (VPN mesh network) on this host";
 
-  # 注意nixos本身支持 services.netbird.clients
-  # 所以虽然netbird是多端共有的服务，但是配置本身无法复用（不能放到base里）
-  services.netbird = {
-    enable = true;
-    package = pkgs.netbird;
+  config = mkIf cfg.enable {
+    # 本配置非必需，services.netbird 本身会安装 netbird，但是需要手动执行 netbird 相关命令时方便
+    environment.systemPackages = with pkgs; [
+      # 注意这里应是 netbird，而非 netbird-client
+      netbird
+    ];
+
+    # 注意 nixos 本身支持 services.netbird.clients；darwin 只有单实例
+    services.netbird = {
+      enable = true;
+      package = pkgs.netbird;
+    };
+
+    environment.shellAliases = {
+      nbs = "netbird";
+    };
   };
 }
