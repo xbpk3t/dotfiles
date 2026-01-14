fb2bdc3e7c071c217597d846044f7a08d6a52bba
xbpk3t <yyzw@live.com>
2026-01-14 17:10:18 +0800

    chore(misc): misc patch

--- home/base/core/default.nix
+++ home/base/core/default.nix
@@ -6,42 +6,54 @@
   imports = [../init.nix] ++ mylib.scanPaths ./.;
 
   # 注意之前这部分在 modules/base 里，以供darwin, nixos 复用。但是实际上应该挪到home里，同样来复用这些pkg
-  home.packages = with pkgs; [
-    # Basic utilities
-    curl
-    wget
+  home.packages = with pkgs;
+    [
+      # Basic utilities
+      curl
+      wget
 
-    tree
+      tree
 
-    zip
-    unzip
+      zip
+      unzip
 
-    # System utilities
-    screen
+      # System utilities
+      screen
 
-    # Development tools
-    gcc
-    gnumake
-    cmake
+      # Development tools
+      gcc
+      gnumake
+      cmake
 
-    dateutils # 操作日期和时间表达式 dateadd、datediff、strptime
+      dateutils # 操作日期和时间表达式 dateadd、datediff、strptime
 
-    # 文件处理
-    tree
-    file
-    which
+      # 文件处理
+      tree
+      file
+      which
 
-    # 压缩工具
-    zip
-    unzip
-    p7zip
-    xz
-    zstd
+      # 压缩工具
+      zip
+      unzip
+      p7zip
+      xz
+      zstd
 
-    gnupg
-    # 挪到base里，因为darwin和nixos都需要使用sops-nix
-    sops
-    age
-    openssh
-  ];
+      gnupg
+      # 挪到base里，因为darwin和nixos都需要使用sops-nix
+      sops
+      age
+      openssh
+    ]
+    ++
+    # 之前放在 hosts/nixos-vps 里，但是实际上VPS并不需要这两个pkg，所以放在homelab里
+    [
+      # https://mynixos.com/nixpkgs/package/nixos-anywhere
+      # 因为可能之后也会用mac作为核心控制端，所以直接放到base里，来多端复用（而非放到专门nixos的nix文件里）
+      # 之所以放在这里，因为无论是nixos还是mac都会引入 home/base/desktop，严格对应关系引用
+      nixos-anywhere
+
+      # 同上，同样只有workstation才有必要引入colmena
+      colmena
+    ];
 }
