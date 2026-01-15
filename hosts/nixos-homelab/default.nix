6f83fff941064125e7c525652d9012078afe1261
xbpk3t <yyzw@live.com>
2026-01-15 10:10:07 +0800

    feat(samba): Add samba service

--- hosts/nixos-homelab/default.nix
+++ hosts/nixos-homelab/default.nix
@@ -62,13 +62,29 @@ in {
 
     homelab = {
       dokploy.enable = true;
+      samba = {
+        enable = true;
+        shareName = "luck";
+        sharePath = "/home/luck";
+        user = "luck";
+        # 这里没办法直接用 pwgen 动态生成密码（我不想把这个塞到sops里），所以直接设置为空串
+        password = "";
+        allowedLan = "192.168.71.0/24";
+        # allowedTailscaleIp = "100.115.38.12/32";
+        # 为了方便，调整为允许整个 tailnet 连接。不会有任何安全问题。
+        # 只是放宽了你本机的本地防火墙/服务访问范围；但 Tailscale 能不能连上 取决于你的 tailnet ACL/SSH policy 和是否在同一个 tailnet。
+        #   - 不在同一个 tailnet 的设备：根本不会有 100.64/10 的地址，也不会通过 Tailscale 直连 → 连不上。
+  # - 在同一个 tailnet，但 ACL 不允许访问该设备/端口：即使你本机放开了网段 → 依然连不上。
+  # - 在同一个 tailnet且 ACL 允许访问：那才会被你本机规则放行。
+        allowedTailscaleIp = "100.64.0.0/10";
+      };
     };
 
     # 私网 homelab：显式关闭内置防火墙
     security.enableFirewall = false;
 
     # 需要远程开发再开启；默认保持关闭即可在 host 覆写
-    extra.vscode-remote.enable = lib.mkDefault false;
+    extra.vscode-remote.enable = lib.mkDefault true;
   };
 
   # system.stateVersion 保持当前主版本
