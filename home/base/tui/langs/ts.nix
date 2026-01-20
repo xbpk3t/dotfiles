4eebd7c02349ae91f64170ef6beed0f69fc76367
xbpk3t <yyzw@live.com>
2026-01-20 23:35:38 +0800

    feat(ai): 添加 opencode 相关配置

--- home/base/tui/langs/ts.nix
+++ home/base/tui/langs/ts.nix
@@ -7,7 +7,7 @@
     nodePackages.serve # https://github.com/vercel/serve 用来preview本地打包好的dist文件（vite可以直接vite preview）
     tsx
 
-    typescript #
+    typescript
     # error: 'ts-node' was removed because it is unmaintained, and since NodeJS 22.6.0+, experimental TypeScript support is built-in to NodeJS.
     # nodePackages.ts-node
     nodePackages.yaml-language-server
@@ -18,4 +18,23 @@
     npm-check # https://github.com/dylang/npm-check 可以认为 npm-check = depcheck + npm-check-updates. 可以用来检查并自动更新dependency，也支持检查unused依赖项. Check for outdated, incorrect, and unused dependencies in package.json.
     npm-check-updates # https://github.com/raineorshine/npm-check-updates 顾名思义，相当于 `npm-check -u`，用来检查pkg版本是否有新版本. 支持brew安装。`ncu -u`
   ];
+
+  # https://mynixos.com/home-manager/options/programs.bun
+  # [2026-01-19] 安装 OMO 需要先安装bun
+  programs.bun = {
+    enable = true;
+    enableGitIntegration = true;
+    settings = {
+      smol = true;
+      telemetry = false;
+      test = {
+        coverage = true;
+        coverageThreshold = 0.9;
+      };
+      install.lockfile = {
+        # Whether to generate a non-Bun lockfile alongside bun.lock. (A bun.lock will always be created.) Currently "yarn" is the only supported value.
+        print = "yarn";
+      };
+    };
+  };
 }
