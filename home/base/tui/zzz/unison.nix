ce6ff91f30d69468f9977c86bfbb7627764f4cf9
xbpk3t <yyzw@live.com>
2026-01-24 23:42:37 +0800

    chore(misc): misc patch

---
+++ home/base/tui/zzz/unison.nix
@@ -0,0 +1,14 @@
+_: {
+  # https://mynixos.com/home-manager/options/services.unison
+  # 
+  # https://mynixos.com/nixpkgs/package/unison
+  # 
+  # 
+  # 
+  # https://mynixos.com/nixpkgs/package/mutagen
+  # https://mynixos.com/nixpkgs/package/mutagen-compose
+  # 
+  # https://github.com/mutagen-io/mutagen
+  # 
+  # 
+}
