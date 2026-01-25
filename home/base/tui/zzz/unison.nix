2d8fac56b89d6219307a68583e57e599511aca2b
xbpk3t <yyzw@live.com>
2026-01-25 11:44:52 +0800

    chore(misc): misc patch

--- home/base/tui/zzz/unison.nix
+++ home/base/tui/zzz/unison.nix
@@ -1,14 +1,16 @@
 _: {
   # https://mynixos.com/home-manager/options/services.unison
-  # 
+  #
   # https://mynixos.com/nixpkgs/package/unison
-  # 
-  # 
-  # 
+  #
+  #
+  #
   # https://mynixos.com/nixpkgs/package/mutagen
   # https://mynixos.com/nixpkgs/package/mutagen-compose
-  # 
+  #
   # https://github.com/mutagen-io/mutagen
-  # 
-  # 
+  #
+  #
+  # TODO: 这种双向同步应该把类似 unison的 pairs 或者 mutagen用来配置同步folder的home.file都做成配置化启用里的配置项。在hosts里配置
+  #
 }
