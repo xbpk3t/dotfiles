cdc28d778beffb9ae36686504a9d69409c09a88f
xbpk3t <yyzw@live.com>
2026-02-25 15:02:18 +0800

    chore(misc): misc patch

--- home/base/desktop/media/default.nix
+++ home/base/desktop/media/default.nix
@@ -1,12 +1,9 @@
-{
-  mylib,
-  pkgs,
-  ...
-}: {
+{mylib, ...}: {
   imports = mylib.scanPaths ./.;
 
-  programs.yt-dlp = {
-    enable = true;
-    package = pkgs.yt-dlp;
-  };
+  # MAYBE: jeepney → secretstorage → yt-dlp 报错。之后判断是否需要，如果不需要，就直接删掉，否则fix
+  #  programs.yt-dlp = {
+  #    enable = true;
+  #    package = pkgs.yt-dlp;
+  #  };
 }
