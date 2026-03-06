c71e06782d65e8463792edcb16783855f64e014a
xbpk3t <yyzw@live.com>
2026-03-06 12:12:19 +0800

    chore(taskfile): skills相关修改，并修改 .taskfile/AGENTS.md

--- home/base/tui/db/default.nix
+++ home/base/tui/db/default.nix
@@ -19,7 +19,26 @@
     #    mycli
     #    pgcli
     #    mongosh
-    #    sqlite
+
+    # https://mynixos.com/nixpkgs/package/sqlite
+    sqlite
+    # https://mynixos.com/nixpkgs/package/sqlite-utils
+    # https://github.com/simonw/sqlite-utils
+    # https://sqlite-utils.datasette.io/
+    sqlite-utils
+
+    # https://mynixos.com/nixpkgs/package/usql
+    # https://github.com/xo/usql
+    # what: 给 pgsql, mysql, oracle, sqlite 之类RDB提供的统一cli工具
+    # usql
+
+    # https://mynixos.com/nixpkgs/package/sqldef
+    # why: 你迟早会改 schema（加列、加索引、加约束）。sqldef 的思路是：维护一份“期望的完整 DDL”，它帮你diff 并把库迁移到目标状态（幂等、适合脚本/CI）。
+    # sqldef
+
+    # https://github.com/simonw/csvs-to-sqlite
+    # https://mynixos.com/nixpkgs/package/csvs-to-sqlite
+    # csvs-to-sqlite
 
     # https://mynixos.com/nixpkgs/package/duckdb
     duckdb
