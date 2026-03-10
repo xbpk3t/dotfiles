8a0a3259cea4d5a9f75ea93b08846d1318579e51
xbpk3t <yyzw@live.com>
2026-03-10 14:03:33 +0800

    feat(flake): 给Determinate Nix配置cache
    ---
    
    才发现 Determinate Nix 本身不支持直接读取在 flake.nix 里配置 substituters 这些配置项

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,7 +1,18 @@
 # Darwin-specific modules
-{mylib, ...}: {
-  imports = mylib.scanPaths ./.;
+{
+  inputs,
+  mylib,
+  ...
+}: {
+  imports =
+    [
+      inputs.determinate.darwinModules.default
+    ]
+    ++ mylib.scanPaths ./.;
 
   # Determinate uses its own daemon to manage the Nix installation that conflicts with nix-darwin’s native Nix management. To turn off nix-darwin’s management of the Nix installation.
   nix.enable = false;
+
+  # 启用 Determinate Nix module，确保 nix.custom.conf / daemon 路径按官方方式管理。
+  determinateNix.enable = true;
 }
