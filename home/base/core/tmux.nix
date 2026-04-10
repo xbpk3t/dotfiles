0fae8070093df11b68d0a1993c4e29ee3c4aed35
xbpk3t <yyzw@live.com>
2026-04-10 23:53:53 +0800

    chore: misc patch

---
+++ home/base/core/tmux.nix
@@ -0,0 +1,13 @@
+_: {
+  # https://mynixos.com/nixpkgs/options/programs.tmux
+  # https://mynixos.com/home-manager/options/programs.tmux
+  # https://mynixos.com/nix-darwin/options/programs.tmux
+
+  # https://x.com/xxm459259/status/2042147729007341837 基本认同该观点，cmux一坨，opensessions 是个 tmux插件，我理解是用tmux来模仿cmux的UI，我觉得这个产品形态和需求是匹配的，
+  # tmux的sidebar插件（类似cmux的感觉），里面实时显示：
+  ## - 所有 tmux sessions 的概览和快速切换
+  ## - AI agent 的状态（正在运行、完成、失败、需要关注等）
+  ## - Git 仓库信息（分支、breadcrumbs 等）
+  ## - 快速跳转回对应终端
+  # https://github.com/ataraxy-labs/opensessions
+}
