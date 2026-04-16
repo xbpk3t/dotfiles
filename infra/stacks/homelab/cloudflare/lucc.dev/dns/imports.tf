13a9873cf446860222ad63c85d83402cd5cfff48
xbpk3t <yyzw@live.com>
2026-04-16 13:38:16 +0800

    feat(infra): 添加 MetAPI 服务的响应DNS记录

--- infra/stacks/homelab/cloudflare/lucc.dev/dns/imports.tf
+++ infra/stacks/homelab/cloudflare/lucc.dev/dns/imports.tf
@@ -2,7 +2,12 @@
 # 这里直接跟随 `locals.dns_records` 自动展开，避免 imports.tf 和 locals.tf 再次漂移。
 
 import {
-  for_each = local.dns_records
+
+  # 否则只有 id != null 的record才会走 import（否则你在配置之前一定不知道id，需要先去 cf dashboard 手动建一遍，在fetch到本地）
+  for_each = {
+    for key, record in local.dns_records : key => record
+    if try(record.id, null) != null
+  }
 
   to = cloudflare_dns_record.records[each.key]
   id = "${local.zone_id}/${each.value.id}"
