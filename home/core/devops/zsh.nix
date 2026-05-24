f623aec1aa37d2f8f564f163f8336a5678223c0e
luck <yyzw@live.com>
2026-05-24 14:57:48 +0800

    fix(hm/zsh): 移除 ll(eza) 的 git-ignore 参数，否则很干扰正常使用

--- home/core/devops/zsh.nix
+++ home/core/devops/zsh.nix
@@ -263,7 +263,6 @@
         "--group-directories-first"
         "--no-quotes"
         "--header" # Show header row
-        "--git-ignore"
         "--icons=always"
         # "--time-style=long-iso" # ISO 8601 extended format for time
         "--classify" # append indicator (/, *, =, @, |)
